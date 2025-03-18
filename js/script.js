bfe16eac0e3db0ce9e156697e54f0599ebe48c73
mannuccimattia <mannuccimattia@gmail.com>
2025-03-18 15:04:58 +0100

    [UPD] Done script.js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//definizione delle variabili
+let num = 0;
+let numFizz = "Fizz";
+let numBuzz = "Buzz";
+
+//ciclo FOR per numeri da 1 a 100
+for(let i=1; i<=100; i++){
+  num = num + 1;
+  
+  if(num % 3 === 0 && num % 5 !== 0){         
+    console.log(numFizz);
+  }
+  else if(num % 5 === 0 && num % 3 !== 0){    
+    console.log(numBuzz);
+  }
+  else if(num % 3 === 0 && num % 5 ===0){     
+    console.log(numFizz+numBuzz);
+  }
+  else{
+    console.log(num);
+  }
+
+}
